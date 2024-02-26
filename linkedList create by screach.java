1367ad79d4011b3e794457f94c0bc0753f33d4d4
Ajay_Kushwaha <130049013+aryan-ajay@users.noreply.github.com>
2024-02-26 22:18:06 +0530

    Update linkedList create by screach.java

--- linkedList create by screach.java
+++ linkedList create by screach.java
@@ -112,14 +112,6 @@ class linkedList {
     public int recSearch(int key) {
         return helper(head, key);
     }
-    public static void print() {
-        Node temp = head;
-        while(temp != null) {
-            System.out.print(temp.data+"->");
-            temp = temp.next;
-        }
-        System.out.println("null");
-    }
 
     public void reverse() {
         Node prev = null;
@@ -134,6 +126,81 @@ class linkedList {
         }
         head = prev;
     }
+
+    public static void removeFromNthnode(int n) {
+        //caliculate size
+        int size = 0;
+        Node temp = head;
+        while(temp != null) {
+            temp = temp.next;
+            size++;
+        }
+
+        if(size == 1) {
+            head = head.next;
+            return;
+        }
+
+        int i=1;
+        int iToFind = size-n;
+        Node prev = head;
+        while (i<iToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public boolean checkPlend() {
+        if(head == null || head.next == null) {
+            return true;
+        }
+        //find mid
+        Node mid = findMid(head);
+
+        //reverse 2nd node 
+        Node prev = null;
+        Node curr = mid;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+
+        // check 
+        while (right != null) {
+            if(left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+    public static void print() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    
     public static void main(String[] args) {
         linkedList ll = new linkedList();
         ll.addFirst(4);
